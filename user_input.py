e8d19de7c9c5afdf4967f549bf4056c39811894a
Hassan Mohammadian <hasssanitman@gmail.com>
2023-03-11 10:00:33 +0330

    cahnge variables name in user_input

--- user_input.py
+++ user_input.py
@@ -1,16 +1,16 @@
 # Getting data from the user
-def get_data(MAX_QUALITY = 5):
-    theList = []
-    for i in range(MAX_QUALITY):
-        quality_input = input(
+def get_data(MAX_PROPERTY = 5):
+    data = []
+    for i in range(MAX_PROPERTY):
+        product_property = input(
             "Please enter quality's name, or type \"End\" to finish (Example: color): ")
-        quality_input = str(quality_input)
-        quality_input = quality_input.lower()
-        if quality_input == "end":
+        product_property = str(product_property)
+        product_property = product_property.lower()
+        if product_property == "end":
             break
-        value_input = input("Please enter the values (Seperate by ','): ")
-        if value_input:
-            value_input = value_input.split(",")
-        temp = {'name': quality_input, 'value': value_input}
-        theList.append(temp)
-    return theList
+        values = input("Please enter the values (Seperate by ','): ")
+        if values:
+            values = values.split(",")
+        temp = {'name': product_property, 'value': values}
+        data.append(temp)
+    return data
